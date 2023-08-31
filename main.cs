76c56e452c02b73f8fba9c306e511a1306c9d4ca
angelll042 <120010011+angelll042@users.noreply.github.com>
2023-08-31 17:10:07 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lucinda Potter");
+        Console.WriteLine("6/24/1992");
+        Console.WriteLine("work 000-000-0101");
+        Console.WriteLine("cell 000-000-0189");
+    }
+}
